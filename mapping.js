c39508c939280b66bcfbcdb72952d9c44951c76b
Michael McMillen <michael.mc09@gmail.com>
2021-02-13 17:44:58 +0000

    Refactored createMarker function

--- mapping.js
+++ mapping.js
@@ -1,14 +1,18 @@
 //Publickey to access MapBox API
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlrZW91dHNpZGUiLCJhIjoiY2pkNXBvYWczMXVraTJxcWp5bHI2MmM4MCJ9.2z_G7gwdmxJWTqjFRHxExg';
 
+//Create map variable
 let map;
 
-var yesButton = document.getElementById('yesButton');
-yesButton.addEventListener('click', yesLocation);
+//Use users location to re-initiate map and stats
+var currentLocation = document.getElementById('myLocation');
+currentLocation.addEventListener('click', myLocation);
 
-var locationButton = document.getElementById('locationButton');
+//Search specific location and reinitiate map and stats
+var locationButton = document.getElementById('searchLocationButton');
 locationButton.addEventListener('click', getCoords);
 
+//Search specific location and reinitiate map and stats when Enter
 var locationInput = document.getElementById('locationInput');
 locationInput.addEventListener('keyup', enterPress = (e) => {
     if(e.keyCode === 13) {
@@ -17,108 +21,76 @@ locationInput.addEventListener('keyup', enterPress = (e) => {
 });
 
 var locationInput = document.getElementById('locationInput');
-
 var longitudeText = document.getElementById('longitudeText');
 var latitudeText = document.getElementById('latitudeText');
 var passTimeText = document.getElementById('passTimeText');
 var peopleInSpaceText = document.getElementById('peopleInSpace');
 
+//Inititate map from generic location (UK)
 noLocation();
 
-function yesLocation() {
-
-    var promise1 = new Promise(function(resolve, reject) {
-
-        navigator.geolocation.getCurrentPosition(function(pos){
-            let lat = pos.coords.latitude
-            let lon = pos.coords.longitude
-            resolve([lon,lat]);
-        },
-        function(err) {
-            if (err.code == err.PERMISSION_DENIED) {
-                console.log(err);
-                createMap("[-0.12838523470829866, 51.504629944543225]")
-            }
-        })
-
-})
-.then(function(value) {
-    createMap(value, '12');
-
-});
-};
-
+//Create map from generic location (UK)
 function noLocation() {
     createMap([-0.12838523470829866, 51.504629944543225])
 };
 
-function createMap(value, zoom = '5') {
-
+//Create map
+function createMap(coords, zoom = '5') {
     if(zoom !== '5') {
-        passTimes(value[0], value[1]);
+        passTimes(coords[0], coords[1]);
     }
-        
+
     peopleInSpace();
 
+    //Create new map
     map = new mapboxgl.Map({
-        
-        container: 'map', // container id
-        style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
-        center: value, // starting position [lng, lat] for Bristol
-        zoom: zoom // starting zoom
+        container: 'map', //Container ID
+        style: 'mapbox://styles/mapbox/outdoors-v11', //Style URL
+        center: coords, //Starting position [lng, lat]
+        zoom: zoom //Starting zoom
     })
+
     startPolling();
 }
 
-function startPolling() {
-    
+//Polling for long and lat position of ISS
+function startPolling() {   
+
+    let marker = createMarker()
+
     setInterval(function() {
-        fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-now.json')
+        fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-now.json') //CORS Anywhere used due to HTTP
         .then(response => response.json())
         .then(coords => {
-            addMarker(coords.iss_position.latitude, coords.iss_position.longitude)
-            longitudeText.textContent = coords.iss_position.longitude;
+            addMarker(marker, coords.iss_position.latitude, coords.iss_position.longitude) //Create ISS marker icon with response coords
+            longitudeText.textContent = coords.iss_position.longitude; //Update long/lat text
             latitudeText.textContent = coords.iss_position.latitude;
         });
     }, 5000);
 }
 
-var el = document.createElement('div');
-el.className = 'marker';
-var marker = new mapboxgl.Marker(el);
- 
-function addMarker(lat, long) {
-    
+//Create element & marker to house ISS icon
+function createMarker() {
+    let element = document.createElement('div');
+    element.className = 'issIcon';
+    let marker = new mapboxgl.Marker(element);
+    return marker;
+}
+
+//Add marker to map and determine movement
+function addMarker(marker, lat, long) {        
     marker.setLngLat([long,lat]);
     marker.addTo(map);
     map.flyTo({
         center: [long, lat],
         zoom: 2.5,
         speed: 0.3,
-        // curve: 0.1,
         essential: true
         });
 }
 
-function passTimes(lon, lat) {
-
-    // Uses 'cors-anywhere' proxy to add Access-Control-Allow-Origin: * to response header
-    let url = new URL('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json')
-    url.search = new URLSearchParams({
-        lat: lat,
-        lon: lon
-})
-
-    fetch(url)
-        .then(response => response.json())
-        .then(passes => {
-            let date = new Date(passes.response[0].risetime*1000);
-            passTimeText.textContent = date.toString();
-        });
-}
-
+//Retrieve number of people in space
 function peopleInSpace() {
-
     // Uses 'cors-anywhere' proxy to add Access-Control-Allow-Origin: * to response header
     fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/astros.json')
         .then(response => response.json())
@@ -127,14 +99,52 @@ function peopleInSpace() {
         });
 }
 
+//Prompt access for browser to access users current location 
+//and use coordinates to initiate map and stats
+function myLocation() {
+
+    //Only create map once browser location is allowed or denied
+    var promise1 = new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(function(pos){
+            let lat = pos.coords.latitude
+            let lon = pos.coords.longitude
+            resolve([lon,lat]);
+        },
+        function(err) {
+            if (err.code == err.PERMISSION_DENIED) {
+                console.log(err);
+                createMap("[-0.12838523470829866, 51.504629944543225]"); //Use set coords if location not found or blocked
+            }
+        })
+    })
+    .then(function(coords) {
+        createMap(coords, '12');
+    });
+};
+
+//Retrieve pass times for location based on users location or search
+function passTimes(lon, lat) {
+    // Uses 'cors-anywhere' proxy to add Access-Control-Allow-Origin: * to response header
+    let url = new URL('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json')
+    url.search = new URLSearchParams({ //Create URL with long and lat parameters
+        lat: lat,
+        lon: lon
+    })
+    fetch(url)
+    .then(response => response.json())
+    .then(passes => {
+        let date = new Date(passes.response[0].risetime*1000); //Convert pass time to readable GMT
+        passTimeText.textContent = date.toString();
+    });
+}
+
+//Get coordinates for searched location
 function getCoords() {
-
     let apiKey = '82845a74c5cd488caf782fd4074540b3';
-
+    //Opencagedata API which converts text location to longitude and latitude values
     fetch('https://api.opencagedata.com/geocode/v1/json?key=' + apiKey + '&q=' + locationInput.value)
-        .then(response => response.json())
-        .then(coords => {
-            // console.log(coords.results[0].geometry.lat, coords.results[0].geometry.lat);
-            createMap([coords.results[0].geometry.lng, coords.results[0].geometry.lat], 12); 
-        });
+    .then(response => response.json())
+    .then(coords => {
+        createMap([coords.results[0].geometry.lng, coords.results[0].geometry.lat], 12);
+    });
 }
